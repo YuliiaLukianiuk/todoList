33c0ed3e7689938a5f714b585af601e9dfa1faa9
YuliiaLukianiuk <lukianiky@gmail.com>
2022-05-26 16:08:10 +0300

    todo list finished

---
+++ src/TodoItem.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function MyInfo() {
+
+
+	return (
+		<div>
+			<ul>
+				<li>First page</li>
+				<li>Second page</li>
+				<li>Third page</li>
+			</ul>
+		</div>)
+}
+
+export default MyInfo;
